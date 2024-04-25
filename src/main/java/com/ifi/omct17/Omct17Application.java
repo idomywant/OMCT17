ab361de9eaef7572ba17283731a51d20dd506493
林冠宇 <idomywant@gmail.com>
2024-04-25 09:47:58 +0800

    commit gitignore

---
+++ src/main/java/com/ifi/omct17/Omct17Application.java
@@ -0,0 +1,13 @@
+package com.ifi.omct17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Omct17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Omct17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifi/omct17/Omct17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifi.omct17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Omct17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
